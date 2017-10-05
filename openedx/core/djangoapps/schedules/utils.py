c9a0c410a14a7487deea013040e045f945900829
Tyler Hallada <thallada@edx.org>
2017-10-05 13:16:12 -0400

    Mixin needs to call super __init__ too

--- openedx/core/djangoapps/schedules/utils.py
+++ openedx/core/djangoapps/schedules/utils.py
@@ -7,6 +7,7 @@ LOG = logging.getLogger(__name__)
 # https://docs.python.org/2/library/logging.html#logging.LoggerAdapter
 class PrefixedDebugLoggerMixin(object):
     def __init__(self, *args, **kwargs):
+        super(PrefixedDebugLoggerMixin, self).__init__(*args, **kwargs)
         self.log_prefix = self.__class__.__name__
 
     def log_debug(self, message, *args, **kwargs):
