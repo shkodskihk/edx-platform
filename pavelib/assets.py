b33a2a6b0c31272dab775655e259f7eaf82e410c
Eric Fischer <efischer@edx.org>
2017-10-05 10:02:28 -0400

    modify COMMON_LOOKUP_PATHS

--- pavelib/assets.py
+++ pavelib/assets.py
@@ -41,10 +41,10 @@ SYSTEMS = {
 
 # Common lookup paths that are added to the lookup paths for all sass compilations
 COMMON_LOOKUP_PATHS = [
-    path("common/static"),
-    path("common/static/sass"),
+    path('common/static'),
+    path('common/static/sass'),
     path('node_modules'),
-    path('node_modules/edx-pattern-library/node_modules'),
+    path('node_modules/@edx'),
 ]
 
 # A list of NPM installed libraries that should be copied into the common
